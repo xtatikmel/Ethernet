026d816ffbb8c93c4943c5162f98324c491b49fa
Guilgamez <c0d3absurd0@gmail.com>
2024-09-24 18:24:34 -0500

    Update

--- Sensorica/Sensorica.ino
+++ Sensorica/Sensorica.ino
@@ -42,7 +42,7 @@ void setup()
     server.begin();           // start to listen for clients
     Serial.begin(9600);       // for debugging
     
-    // initialize SD card
+    // inicializando SD card
     Serial.println("Inicializacion targeta SD...");
     if (!SD.begin(4)) {
         Serial.println("ERROR - Fallo Inicializacion targeta SD!");
@@ -55,4 +55,49 @@ void setup()
         return;  // can't find index file
     }
     Serial.println("SUCCESS - Archivo index.htm encontrado.");
+}
+
+void loop()
+{
+    EthernetClient client = server.available();  // intenta conseguir cliente
+
+    if (client) {  // got client?
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {   // client data available to read
+                char c = client.read(); // read 1 byte (character) from client
+                // last line of client request is blank and ends with \n
+                // respond to client only after last line received
+                if (c == '\n' && currentLineIsBlank) {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connection: close");
+                    client.println();
+                    // send web page
+                    webFile = SD.open("index.htm");        // open web page file
+                    if (webFile) {
+                        while(webFile.available()) {
+                            client.write(webFile.read()); // send web page to client
+                        }
+                        webFile.close();
+                    }
+                    break;
+                }
+                // every line of text received from the client ends with \r\n
+                if (c == '\n') {
+                    // last character on line of received text
+                    // starting new line with next character read
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    // a text character was received from client
+                    currentLineIsBlank = false;
+                }
+            } // end if (client.available())
+        } // end while (client.connected())
+        delay(1);      // give the web browser time to receive the data
+        client.stop(); // close the connection
+    } // end if (client)
+
 }
