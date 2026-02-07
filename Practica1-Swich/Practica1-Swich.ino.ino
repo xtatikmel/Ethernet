025636132d37e188b5f003c5b3a565d1a8de8c77
xtatikmel <c0d3absurd0@gmail.com>
2026-02-07 00:03:26 -0500

    Update
    Organizando carpetas

---
+++ Practica1-Swich/Practica1-Swich.ino.ino
@@ -0,0 +1,96 @@
+/*--------------------------------------------------------------
+  Program:      eth_websrv_switch
+
+  Description:  Arduino web server shows the state of a switch
+                on a web page. Does not use the SD card.
+  
+  Hardware:     Arduino Uno and official Arduino Ethernet
+                shield. Should work with other Arduinos and
+                compatible Ethernet shields.
+                
+  Software:     Developed using Arduino 1.0.3 software
+                Should be compatible with Arduino 1.0 +
+  
+  References:   - WebServer example by David A. Mellis and 
+                  modified by Tom Igoe
+                - Ethernet library documentation:
+                  http://arduino.cc/en/Reference/Ethernet
+
+  Date:         12 January 2013
+ 
+  Author:       W.A. Smith, http://startingelectronics.org
+--------------------------------------------------------------*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(10, 0, 0, 20); // IP address, may need to change depending on network
+EthernetServer server(80);  // create a server at port 80
+
+void setup()
+{
+    Ethernet.begin(mac, ip);    // initialize Ethernet device
+    server.begin();             // start to listen for clients
+    pinMode(3, INPUT);  // input pin for switch
+}
+
+void loop()
+{
+    EthernetClient client = server.available();  // try to get client
+
+    if (client) {  // got client?
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {   // client data available to read
+                char c = client.read(); // read 1 byte (character) from client
+                // last line of client request is blank and ends with \n
+                // respond to client only after last line received
+                if (c == '\n' && currentLineIsBlank) {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connnection: close");
+                    client.println();
+                    // send web page
+                    client.println("<!DOCTYPE html>");
+                    client.println("<html>");
+                    client.println("<head>");
+                    client.println("<title>Arduino Read Switch State</title>");
+                    client.println("<meta http-equiv=\"refresh\" content=\"1\">");
+                    client.println("</head>");
+                    client.println("<body>");
+                    client.println("<h1>Switch</h1>");
+                    client.println("<p>State of switch is:</p>");
+                    GetSwitchState(client);
+                    client.println("</body>");
+                    client.println("</html>");
+                    break;
+                }
+                // every line of text received from the client ends with \r\n
+                if (c == '\n') {
+                    // last character on line of received text
+                    // starting new line with next character read
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    // a text character was received from client
+                    currentLineIsBlank = false;
+                }
+            } // end if (client.available())
+        } // end while (client.connected())
+        delay(1);      // give the web browser time to receive the data
+        client.stop(); // close the connection
+    } // end if (client)
+}
+
+void GetSwitchState(EthernetClient cl)
+{
+    if (digitalRead(3)) {
+        cl.println("<p>ON</p>");
+    }
+    else {
+        cl.println("<p>OFF</p>");
+    }
+}
